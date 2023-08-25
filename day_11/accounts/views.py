2395b2c4a1e748c993d07c4ab45913a3a6c672b1
dotjarvis <johnsonmakings@gmail.com>
2023-08-26 01:14:44 +0300

    11th commit
    Full update on inlineform

--- day_11/accounts/views.py
+++ day_11/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 
 # Create your views here.
 from .models import *
@@ -36,17 +37,18 @@ def customer(request, pk_test):
     return render(request, 'accounts/customer.html', context)
 
 
-def createOrder(request,pk):
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
     customer = Customer.objects.get(id=pk)
-    form = OrderForm(initial={'customer':customer})
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
 
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = {'formset': formset}
     return render(request, 'accounts/create_order.html', context)
 
 
